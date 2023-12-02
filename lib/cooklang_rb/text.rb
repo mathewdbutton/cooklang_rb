8e1cf5f0dfd05b44e2b7cc726f9cd818c3769de5
Mathew Button <mat@pushbutton.xyz>
2023-12-02 22:59:32 +1100

    fix: handle when final step is missing newline
    Prior to this change, if the final line of the recipe didn't contain a
    new line character, the parser would hang. This would occur when there
    are no more TAGs to parse and it's just straight text between the
    buffer head and the end of the buffer. If there were no more tags the
    rest of the text was returned, however the buffer position wasn't
    correctly updated to point to the end of the buffer.
    
    This change will update the buffers position to point to the end of the
    string when there are no more tags left.

--- lib/cooklang_rb/text.rb
+++ lib/cooklang_rb/text.rb
@@ -10,9 +10,15 @@ module CooklangRb
 
     def self.parse_from(buffer)
       text = buffer.scan_until TAGS
-      text = text&.sub(TAGS, "") || buffer.rest
-      buffer.pos = buffer.pos - 1 unless buffer.eos?
-      new text.chomp
+      if text.nil?
+        text = buffer.rest
+        buffer.pos = buffer.rest_size + buffer.pos
+        new text.chomp
+      else
+        text = text.sub(TAGS, "")
+        buffer.pos = buffer.pos - 1 unless buffer.eos?
+        new text.chomp
+      end
     end
 
     def initialize(value)
